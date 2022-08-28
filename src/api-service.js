5282a8028f996bda6031697af049c923e96d7d2b
Eremey2410 <2410esv@gmail.com>
2022-08-28 18:34:05 +0300

    done

--- src/api-service.js
+++ src/api-service.js
@@ -1,40 +1,36 @@
 import axios from 'axios';
+const axios = require('axios');
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '29482486-4af73e7428fa82566a6b382e2';
-let page = 1;
-
-// function getData(name, page) {
-//   axios
-//     .get(
-//       `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-//     )
-//     .then(function (response) {
-//       // обработка успешного запроса
-//       console.log(response);
-//     })
-//     .catch(function (error) {
-//       // обработка ошибки
-//       console.log(error);
-//     });
-// }
-
-async function getData(name, page) {
-  const response = await axios.get(
-    `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-  );
-  //   return response;
+export default class NewApiService {
+  // хранение запроса + номер группы
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  getData() {
+    return axios
+      .get(
+        `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
+      )
+      .then(response => {
+        // обработка успешного запроса
+        console.log(response.data.hits);
+        this.incrementPage();
+        return response;
+        // return response.data.hits;
+      });
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
-
-export default { getData };
-// axios
-//   .get(
-//     `${BASE_URL}?key=${API_KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
-//   )
-//   .then(function (response) {
-//     // обработка успешного запроса
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // обработка ошибки
-//     console.log(error);
-//   });
--- src/index.js
+++ src/index.js
@@ -1,27 +1,120 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import API from './api-service';
+import NewApiService from './api-service';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
   btnLoadMore: document.querySelector('.load-more'),
+  gallery: document.querySelector('.gallery'),
 };
+const newsApiService = new NewApiService();
+// console.log(newsApiService);
 
 refs.searchForm.addEventListener('submit', onSearchForm);
 refs.btnLoadMore.addEventListener('click', onLoadMore);
 
-let searchQuery = '';
-
 function onSearchForm(event) {
   event.preventDefault();
 
-  searchQuery = event.currentTarget.elements.searchQuery.value;
+  clearMarkup();
 
-  API.getData(searchQuery);
-  console.log(API.getData());
+  newsApiService.query = event.currentTarget.elements.searchQuery.value;
+  if (newsApiService.query !== '') {
+    newsApiService.resetPage();
+    console.log(newsApiService);
+    newsApiService
+      .getData()
+      .then(response => {
+        console.log(response);
+        Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+
+        return response.data.hits;
+      })
+      .then(data => addMarkup(data), removeClassIsHidden())
+      .catch(error => {
+        console.log(error);
+      });
+  } else {
+    Notify.failure(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    addClassIsHidden();
+    return;
+  }
 }
 
 function onLoadMore() {
-  API.getData(searchQuery);
+  newsApiService
+    .getData()
+    .then(response => {
+      return response.data.hits;
+    })
+    .then(addMarkup)
+    .catch(error => {
+      console.log(error);
+      addClassIsHidden();
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    });
+  smoothScroll();
+}
+function addMarkup(data) {
+  if (data.length === 0) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+  const elements = data;
+  const markup = elements
+    .map(
+      element =>
+        `<div class="photo-card">
+        <a class="gallery-item" href = "${element.largeImageURL}" >
+    <img class="gallery-image" src="${element.webformatURL}" 
+    alt="${element.tags}" loading="lazy" /></a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        ${element.likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        ${element.views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+        ${element.comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        ${element.downloads}
+      </p>
+    </div>
+  </div>`
+    )
+    .join('');
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+  // console.log(elements);
+
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.refresh();
+}
+function clearMarkup() {
+  refs.gallery.innerHTML = '';
+}
+function removeClassIsHidden() {
+  refs.btnLoadMore.classList.remove('is-hidden');
+}
+function addClassIsHidden() {
+  refs.btnLoadMore.classList.add('is-hidden');
+}
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
