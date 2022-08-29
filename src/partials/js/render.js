bed538b8a919fd20a54f9194bfec98d9152381da
Eremey2410 <2410esv@gmail.com>
2022-08-29 10:46:29 +0300

    add notify in render.js

--- src/partials/js/render.js
+++ src/partials/js/render.js
@@ -1,5 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 let gallery = new SimpleLightbox('.gallery a');
 
 export default function addMarkup(data) {
